a4a3060ee469f7456d40186835890b142cea4574
Andrey Shedko <ashedko@localhost.localdomain>
2016-10-05 16:53:11 +0300

    Done with tokenize.pl

--- homeworks/calculator/lib/tokenize.pl
+++ homeworks/calculator/lib/tokenize.pl
@@ -6,8 +6,9 @@
 В случае ошибки в выражении функция должна вызывать die с сообщением об ошибке
 
 Знаки '-' и '+' в первой позиции, или после другой арифметической операции стоит воспринимать
-как унарные и можно записывать как "U-" и "U+"
+как унарные и можно записывать как U-" и "U+"
 
+Программа должна читать выражения из стандартного ввода.
 Стоит заметить, что после унарного оператора нельзя использовать бинарные операторы
 Например последовательность 1 + - / 2 невалидна. Бинарный оператор / идёт после использования унарного "-"
 
@@ -22,16 +23,69 @@ BEGIN{
 		package experimental;
 		use warnings::register;
 	}
+  $|++;     # Enable autoflush on STDOUT
+	$, = " "; # Separator for print x,y,z
+	$" = " "; # Separator for print "@array";
 }
 no warnings 'experimental';
+use constant EXP=>1;
+use constant OP=>2;
+use constant UN=>3;
+
 
 sub tokenize {
 	chomp(my $expr = shift);
 	my @res;
+  my @chunks = split(m{([-+*/()])}, $expr);
+  my $prev = "";
+  my $state = 0;
+  for my $c (@chunks){
+      given ($c){
+          when (/^\s*$/) {} # то-же самое
+          when (/\d*.?\d+E/){ # элемент содержит число в эксп. записи
+              $prev.=$c;
+              $state = 1;
+          }
+          when (/\d*.?\d+/) { # элемент содержит число
+              if ($state==EXP){
+                  $prev.=$c;
+                  push(@res,$prev);
+                  $prev="";
+                  $state=0;
+              }
+              else{push(@res,$c);}
+          }
+          when ([ '+','-' ]){ # элемент "+" или "-"
+              if ($state==EXP){
+                  $prev.=$c;
+              }
+              elsif($state==OP or $state==UN){push(@res,("U".$c));$state=UN}
+              else {push(@res,$c);$state=OP}
+          }
+          when(['(',')']){
+              push(@res,$c);
+              $state=0;
+          }
+          when(['*','/']){
+              unless ($state){
+                  push(@res,$c);
+              }
+              else {
+                  die "incorrect operators: '$_'";
+              }
 
-	# ...
-
+          }
+          default {
+              die "Bad: '$_'";
+}
+      }
+  }
 	return \@res;
 }
-
-1;
+if ($__FILE__ eq $__MAIN__){
+    use Data::Dumper;
+    while (my $expression = <>) {
+        next if $expression =~ /^\s*$/;
+        print Dumper(tokenize($expression));
+    }
+}
